2eedcac20bff8aa35677c2cda60b01c8786015e4
JavierIMV98 <moyavalerio.ji@gmail.com>
2024-11-05 17:23:41 -0300

    fix bug no such table en on upgrade

--- app/src/main/java/cl/javier/pool/db/DbHelper.java
+++ app/src/main/java/cl/javier/pool/db/DbHelper.java
@@ -7,7 +7,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 public class DbHelper extends SQLiteOpenHelper {
-    private static final int DATABASE_VERSION = 4;
+    private static final int DATABASE_VERSION = 5;
     private static final String DATABASE_NAME = "pool.db";
     public static final String TABLE_MESAS = "mesas";
     public static final String TABLE_HISTORIAL_MESAS = "historial_mesas";
@@ -18,14 +18,18 @@ public class DbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL("CREATE TABLE " + TABLE_MESAS + "("+ "id INTEGER PRIMARY KEY AUTOINCREMENT,"+"nro INTEGER NOT NULL UNIQUE,"+"hora_inicio TEXT NOT NULL," +"precio INTEGER NOT NULL,"+"extras INTEGER" + ")");
-        sqLiteDatabase.execSQL("CREATE TABLE " + TABLE_HISTORIAL_MESAS +"("+ "id INTEGER PRIMARY KEY AUTOINCREMENT,"+"nro INTEGER NOT NULL,"+"hora_inicio TEXT NOT NULL," + "hora_final TEXT NOT NULL,"+"precio INTEGER NOT NULL,"+"valor INTEGER," + "fecha TEXT NOT NULL" + ")");
+
+
+        sqLiteDatabase.execSQL("CREATE TABLE " + TABLE_MESAS + "(" + "id INTEGER PRIMARY KEY AUTOINCREMENT," + "nro INTEGER NOT NULL UNIQUE," + "hora_inicio TEXT NOT NULL," + "precio INTEGER NOT NULL," + "extras INTEGER" + ")");
+        sqLiteDatabase.execSQL("CREATE TABLE " + TABLE_HISTORIAL_MESAS + "(" + "id INTEGER PRIMARY KEY AUTOINCREMENT," + "nro INTEGER NOT NULL," + "hora_inicio TEXT NOT NULL," + "hora_final TEXT NOT NULL," + "precio INTEGER NOT NULL," + "valor INTEGER," + "fecha TEXT NOT NULL" + ")");
+
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        sqLiteDatabase.execSQL("DROP TABLE " + TABLE_MESAS);
-        sqLiteDatabase.execSQL("DROP TABLE " + TABLE_HISTORIAL_MESAS);
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_MESAS);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_HISTORIAL_MESAS);
+
         onCreate(sqLiteDatabase);
     }
 }
